baed24005d05221a4b69a5344f64dfad4a2b01f2
Leonardo Zambrana <leonardozambrana1@gmail.com>
2023-06-14 21:45:39 -0300

    feat(PushNotification): implementa notificacao

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import AppRoutes from "./src/routes/app.routes";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import {
+  handleTokenPush,
+  useNotificationHandler,
+  setNotificationChannel,
+  requestNotificationPermissions,
+} from "./src/global/Notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowAlert: true,
+  }),
+});
 
 export default function App() {
-  return (
-    <AppRoutes/>
-  );
+  const notification  = useNotificationHandler();
+
+  useEffect(() => {
+    async function configureNotifications() {
+      await setNotificationChannel();
+      const permissionGranted = await requestNotificationPermissions();
+      if (permissionGranted) {
+        await handleTokenPush();
+      }
+    }
+    configureNotifications();
+  }, []);
+
+  useEffect(() => {
+    console.log(notification);
+  }, [notification]);
+
+  return <AppRoutes />;
 }
