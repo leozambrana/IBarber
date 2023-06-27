818fccf019bde9317c5dd9b404c65e775a18cb8d
Leonardo Zambrana <leonardozambrana1@gmail.com>
2023-06-27 16:37:36 -0300

    feat(IconeServicos): implementa icones para tela de servicos

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { useEffect } from "react";
 import AppRoutes from "./src/routes/app.routes";
 import * as Notifications from "expo-notifications";
 
-import { useContext, useEffect, useState } from "react";
 import { ThemeProvider as CustomThemeContext } from "./src/global/styles/themeProvider";
 
 Notifications.setNotificationHandler({
